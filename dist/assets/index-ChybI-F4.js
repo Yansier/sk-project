2f4834f2a3c0af7ef64b752c6ba1710d07fe176d
Yansier <ledermanncaravetta968@gmail.com>
2025-04-10 22:09:09 +0800

    inner dist

--- dist/assets/index-ChybI-F4.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"./request-C0k_ht0T.js";function u(){return r({url:"/app/product/category/tree"})}function p(t){return r({url:"/app/product/item/list",params:t})}function o(t){return r({url:`/app/product/item/${t}`})}function a(){return r({url:"/app/product/item/index"})}export{o as a,u as c,a as h,p};
